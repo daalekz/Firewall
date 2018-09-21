669662e38212ca6a50cb7f3b368ead35108f816f
Daniel Coady <daniel.coady@outlook.com.au>
2018-09-21 23:23:08 +1000

    Added simple AI point to point pathing

---
+++ Assets/Scripts/AIController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIController : MonoBehaviour
+{
+	GameController gc;
+
+	int nodeIndex = 0;
+
+	[Header("Movement")]
+	public float speed = 0.5f;
+	public float turnDeadzone = 0.3f;
+	Vector3 direction, heading;
+
+	// Use this for initialization
+	void Start ()
+	{
+		gc = GameController.instance;
+
+		// Set the initial direction the AI will move in
+		direction = gc.navPoints[1].transform.position - gc.navPoints[0].transform.position;
+		direction = direction / direction.magnitude;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position += (direction * speed) * Time.deltaTime;
+	}
+
+    // Handle changing the AI's direction when it hits a corner
+	void ChangeDirection ()
+	{
+		nodeIndex++;
+		direction = gc.navPoints[nodeIndex + 1].transform.position - transform.position;
+		direction = direction / direction.magnitude;
+	}
+
+	void OnTriggerEnter2D (Collider2D col)
+	{
+		transform.position = col.transform.position;
+		ChangeDirection();
+	}
+}
