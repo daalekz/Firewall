bee0d822f31453d8e69be01d92469694fe49873c
Jeremy <jeremy.v.maxwell@bigpond.com>
2018-10-12 00:22:53 +1100

    functional main menu

---
+++ Assets/MainMenuController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+	public void StartGame()
+	{
+		//ADD GAME SCENE HERE
+		//SceneManager.LoadScene("game schene name here");
+		Debug.Log("Play button clicked");
+	}
+}
