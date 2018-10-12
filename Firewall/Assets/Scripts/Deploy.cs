8144c9e9f10914dbdc3693f4bb33835f96b7aa26
Tim <tim.keesman@gmail.com>
2018-10-12 23:03:05 +1100

    ability to select and delete towers

--- Firewall/Assets/Scripts/Deploy.cs
+++ Firewall/Assets/Scripts/Deploy.cs
@@ -1,17 +1,103 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System;
 
-//https://unity3d.college/2017/10/08/simple-unity3d-snap-grid-system/
+public class Tower
+{
+    private Vector3 _position;
+    private GameObject rendered_tower;
+    private bool _selected;
+
+    public Tower(int x, int y)
+    {
+        rendered_tower = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        _position.x = x;
+        _position.y = y;
+        _position.z = 0;
+        var shape_color = rendered_tower.GetComponent<Renderer>().material.color;
+        shape_color.a = 0;
+    }
+
+    public Tower (Vector3 position)
+    {
+        rendered_tower = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        _position = position;
+        var shape_color = rendered_tower.GetComponent<Renderer>().material.color;
+        shape_color.a = 0;
+    }
+
+    public void Render()
+    {
+        rendered_tower.transform.position = _position;
+
+        if (_selected)
+        {
+            rendered_tower.GetComponent<Renderer>().material.color = Color.yellow;
+        }
+        else
+        {
+            rendered_tower.GetComponent<Renderer>().material.color = Color.cyan;
+        }
+    }
 
 
-public class Map : MonoBehaviour
+    public bool Selected
+    {
+        get
+        {
+            return _selected;
+        }
+
+        set
+        {
+            _selected = value;
+        }
+    }
+    
+    public GameObject TowerObj
+    {
+        get
+        {
+            return rendered_tower;
+        }
+
+        set
+        {
+            rendered_tower = value;
+        }
+    }
+
+    public Vector3 Position
+    {
+        get
+        {
+            return _position;
+        }
+
+        //if you want to move tower position!
+        set
+        {
+            _position = value;
+        }
+    }
+}
+
+/*
+    Summary:
+    Class is used to store the various information regarding the game tiles
+*/
+public class Map
 {
     private int _num_tiles;
     private int _tiles_wide;
     private int _tiles_high;
     private List<MapTile> tile_list;
+    private List<Tower> map_towers;
 
+
+    //constructor takes number of tiles for the map grid
+    //the number of tiles wide and the number of tiles high
     public Map(int num_tiles, int width, int height)
     {
         _num_tiles = num_tiles;
@@ -19,8 +105,10 @@ public class Map : MonoBehaviour
         _tiles_wide = width;
 
         tile_list = new List<MapTile>();
+        map_towers = new List<Tower>();
     }
 
+    //returns the objects private list of tiles present in the game
     public List<MapTile> Map_Tiles{
         get
         {
@@ -43,6 +131,14 @@ public class Map : MonoBehaviour
         set
         {
             _num_tiles = value;
+        }
+    }
+
+    public List<Tower> Map_Towers
+    {
+        get
+        {
+            return map_towers;
         }
     }
 
@@ -73,29 +169,94 @@ public class Map : MonoBehaviour
     }
 }
 
+/*
+    Class taken and modified from: https://unity3d.college/2017/10/08/simple-unity3d-snap-grid-system/
+*/
 public class Deploy : MonoBehaviour
 {
+    //needs to get the data from camera and grid (hence they are passed into script via the object)
     public Grid_Setup grid;
     public Camera cam;
-
+    private static Tower selected_tower;
 
     private void Awake()
     {
         grid = FindObjectOfType<Grid_Setup>();
     }
 
+    //draws sphere when clicked on a given tile
     private void Update()
     {
+        RaycastHit hitInfo;
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
         if (Input.GetMouseButtonDown(0))
         {
-            RaycastHit hitInfo;
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-
             if (Physics.Raycast(ray, out hitInfo))
             {
                 PlaceCubeNear(hitInfo.point);
             }
         }
+        
+        if (Input.GetMouseButton(0) && Input.GetKey(KeyCode.LeftControl))
+        {
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                var finalPosition = grid.GetNearestPointOnGrid(hitInfo.point);
+                finalPosition.z = 0;
+
+                if (grid.GameMap.Map_Towers != null)
+                {
+                    if (selected_tower == null)
+                    {
+                        DeployTools.SelectTower(grid.GameMap.Map_Towers, finalPosition).Selected = true;
+                        selected_tower = DeployTools.SelectTower(grid.GameMap.Map_Towers, finalPosition);
+                    }
+                    else
+                    {
+                        DeployTools.GetTower(grid.GameMap.Map_Towers, selected_tower).Selected = false;
+                        selected_tower = null;
+                    }
+                }
+            }
+        }
+
+
+        if (Input.GetKey(KeyCode.Delete))
+        {
+            if (selected_tower != null)
+            {
+          
+                grid.GameMap.Map_Towers.Remove(DeployTools.GetTower(grid.GameMap.Map_Towers, selected_tower));
+                DeployTools.Manage_Tile_Type(selected_tower.Position, TileType.empty, grid.GameMap.Map_Tiles);
+
+
+                Destroy(selected_tower.TowerObj);
+                selected_tower = null;
+                
+            }
+        }
+
+        RenderTowers();
+    }
+
+    private void HoverPlacement(Vector3 mousePosition)
+    {
+            var finalPosition = grid.GetNearestPointOnGrid(mousePosition);
+
+           //GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = finalPosition;
+
+            MapTile temp_tile = DeployTools.SearchTiles(finalPosition, grid.GameMap.Map_Tiles);
+            
+            if (temp_tile == null)
+            {
+                return;
+            }
+
+            GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            sphere.transform.position = finalPosition;
+            var shape_color = sphere.GetComponent<Renderer>().material.color;
+            shape_color.a = 0.5f;
     }
 
     private void PlaceCubeNear(Vector3 clickPoint)
@@ -105,21 +266,30 @@ public class Deploy : MonoBehaviour
 
         MapTile temp_tile = DeployTools.SearchTiles(finalPosition, grid.GameMap.Map_Tiles);
 
+        //gets the given tile for the position that the user clicked, and checks that a tower can actually be placed there
         if (temp_tile.Type == TileType.empty)
         {
             DeployTools.Manage_Tile_Type(finalPosition, TileType.turret, grid.GameMap.Map_Tiles);
-            GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            
-            sphere.transform.position = finalPosition;
 
-            sphere.GetComponent<Renderer>().material.color = Color.cyan;
-
-            DeployTools.Manage_Tile_Type(finalPosition, TileType.turret, grid.GameMap.Map_Tiles);
-
+            if (grid.GameMap.Map_Towers != null)
+            {
+                grid.GameMap.Map_Towers.Add(new Tower(finalPosition));
+            }
         }
         else
         {
 
+        }
+    }
+
+    private void RenderTowers()
+    {
+        if (grid.GameMap.Map_Towers != null)
+        {
+            foreach (Tower tower in grid.GameMap.Map_Towers)
+            {
+                tower.Render();
+            }
         }
     }
 }
--- Firewall/Assets/Scripts/Grid_Setup.cs
+++ Firewall/Assets/Scripts/Grid_Setup.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using System.IO;
 using System;
 
+//enum represents the types of objects that can be present on the tile
 public enum TileType
 {
     path,
@@ -11,8 +12,6 @@ public enum TileType
     turret,
     empty
 }
-
-//https://unity3d.college/2017/10/08/simple-unity3d-snap-grid-system/
 
 public class MapTile 
 {
@@ -59,6 +58,8 @@ public class MapTile
     }
 }
 
+
+//class is a number of static methods that can used for data processing (i.e. not object specific) 
 public class DeployTools
 {
     public static MapTile SearchTiles(Vector3 Search_Position, List<MapTile> Search_List)
@@ -87,20 +88,39 @@ public class DeployTools
         }
     }
 
-    public static TileType EnumFromInt()
+    public static Tower SelectTower(List<Tower> towers, Vector3 Search_Position)
     {
-        return TileType.empty;
+        foreach(Tower tower in towers)
+        {
+            if (tower.Position.x == Search_Position.x && tower.Position.y == Search_Position.y)
+            {
+                return tower;
+            }
+        }
+
+        return null;
+    }
+
+    public static Tower GetTower(List<Tower> towers, Tower selected_tower)
+    {
+        foreach (Tower tower in towers)
+        {
+            if (selected_tower == tower)
+            {
+                return tower;
+            }
+        }
+
+        return null;
     }
 }
 
+/*
+Class taken and modified from:
+https://unity3d.college/2017/10/08/simple-unity3d-snap-grid-system/
+*/
 public class Grid_Setup : MonoBehaviour {
-    [SerializeField]
     private float size = 1f;
-    [SerializeField]
-    private float cells_high = 1f;
-    [SerializeField]
-    private float cells_wide = 1f;
-
     private List<MapTile> tiles = new List<MapTile>();
 
     private Map game_map;
@@ -110,32 +130,41 @@ public class Grid_Setup : MonoBehaviour {
         game_map = GetMapData();
         OnDraw(game_map);
     }
-
-
-
-
+    
     // Update is called once per frame
     void Update() {
 
     }
+    
 
+   //reads through a data file to create the game map
     public Map GetMapData()
     {
-        Debug.Log(Directory.GetCurrentDirectory());
         string file_path = "Assets/Data/sample_map.txt";
 
         int num_cells, width, height;
 
+        /*
+         * Note regarding file structure:
+         * First Line: Total Number of Cells
+         * Second Line: Number of horizontal blocks
+         * Third Line: Number of vertical block
+         * 4th - EOF: tile type (number that is converted to enum) - goes left to right, top to bottom
+         * (hence last line on file represent the bottom right tile
+        */
+        
         using (StreamReader sr = new StreamReader(file_path, true))
         {
-
             num_cells = Convert.ToInt32(sr.ReadLine());
 
             width = Convert.ToInt32(sr.ReadLine());
 
             height = Convert.ToInt32(sr.ReadLine());
 
+            //creates game map that will be returned
             Map method_map = new Map(num_cells, width, height);
+
+            //gets the tile x and y values, so when it called, it can be rendered
             for (float y = height; y > 0; y -= size)
             {
                 for (float x = 0; x < width; x += size)
@@ -145,7 +174,7 @@ public class Grid_Setup : MonoBehaviour {
                     if ((line = sr.ReadLine()) != null)
                     {
                         int line_value = Convert.ToInt32(line);
-                        Debug.Log(line_value);
+                        //creates a tile (from the calculated values and file data) and adds it to list within map obj
                         method_map.Map_Tiles.Add(new MapTile((TileType)line_value, point));
                     }
                     else
@@ -155,15 +184,13 @@ public class Grid_Setup : MonoBehaviour {
                 }
             }
             // Read the stream to a string, and write the string to the console.
-
             return method_map;
-           
         }
     }
-
+    
+    //for a given mouse position, the closest grid point is found
     public Vector3 GetNearestPointOnGrid(Vector3 position)
     {
-
         position -= transform.position;
 
         int xCount = Mathf.RoundToInt(position.x / size);
@@ -180,6 +207,7 @@ public class Grid_Setup : MonoBehaviour {
         return result;
     }
 
+    //draws the game squares 
     void OnDraw(Map map)
     {
         foreach(MapTile tile in map.Map_Tiles)
@@ -188,6 +216,7 @@ public class Grid_Setup : MonoBehaviour {
             cube.transform.position = tile.Position;
             cube.transform.localScale = new Vector3(0.9f, 0.9f, 0.1f);
 
+            //draws differently colored squares depending on the enum value
             switch (tile.Type)
             {
                 case (TileType.empty):
@@ -201,7 +230,6 @@ public class Grid_Setup : MonoBehaviour {
                 case (TileType.terrain):
                     cube.GetComponent<Renderer>().material.color = Color.green;
                     break;
-
             }
         }
     }
@@ -211,6 +239,22 @@ public class Grid_Setup : MonoBehaviour {
         get
         {
             return game_map.Map_Tiles;
+        }
+    }
+
+    public int Width
+    {
+        get
+        {
+            return game_map.Width;
+        }
+    }
+
+    public int Height
+    {
+        get
+        {
+            return game_map.Height;
         }
     }
 
