07af032ebab1999435d648d5a3abdff769a8d293
Jeremy <jeremy.v.maxwell@bigpond.com>
2018-10-12 16:38:54 +1100

    Added empty slide out menu

---
+++ Assets/MainMenuaController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuaController : MonoBehaviour {
+
+	public Transform Active, Inactive;
+	private Transform Target;
+
+	void Start()
+	{
+		Target = Inactive;
+	}
+
+	void Update()
+	{
+		transform.position = Vector3.Lerp(transform.position, Target.position, 0.1f);
+	}
+
+	public void ToggleMenu()
+	{
+		if (Target == Active)
+		{
+			Target = Inactive;
+		}
+		else
+		{
+			Target = Active;
+		}
+	}
+}
