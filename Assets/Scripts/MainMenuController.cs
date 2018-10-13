9d8d824ea1f960913ea329c28b9955baa282c981
Jeremy <jeremy.v.maxwell@bigpond.com>
2018-10-14 00:26:52 +1100

    Added comments and corrected spelling error in file name

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 public class MainMenuaController : MonoBehaviour {
 
+	//Active and inactive targets for the panel position
 	public Transform Active, Inactive;
+
+	//Current target of the panel
 	private Transform Target;
+
 
 	void Start()
 	{
+		//Target is set to the inactive position on startup
 		Target = Inactive;
 	}
 
 	void Update()
 	{
+		//Moves the panel towards the target position
 		transform.position = Vector3.Lerp(transform.position, Target.position, 0.1f);
 	}
 
+	//Changes the target position of the pannel to active or inactive
 	public void ToggleMenu()
 	{
 		if (Target == Active)
